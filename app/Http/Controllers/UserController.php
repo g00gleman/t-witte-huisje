ddde9741e2caa55faef1d16bb21bc808b4587116
githubBram01 <93133849+githubBram01@users.noreply.github.com>
2022-11-25 13:30:24 +0100

    Initial commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function loginform()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //      Return registerFrom
+        return view('User/register');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //      Valideren inputs
+        $request->validate([
+            'name' => 'required|min:3|max:255',
+            'email' => 'required|min:3|max:255|unique:users',
+            'password' => 'required|min:8|max:50|confirmed',
+            'password_confirmation' => 'required|min:8|max:50'
+        ]);
+
+        //      Opslaan inputs in Usertable
+        $storeUser = new User();
+        $storeUser->name = $request->get('name');
+        $storeUser->email = $request->get('email');
+        $storeUser->password = $request->get('password');
+        $storeUser->save();
+
+        //      Return Home
+        return redirect('/home');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function check(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
